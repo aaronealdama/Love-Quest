74fe3ddc0ebcda215b7647a9631abf1e97324782
Aaron Ealdama <aaronealdama@gmail.com>
2020-04-06 15:03:51 -0500

    finished logging in functinality

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
   // Event Listeners
-  $(".login-form").on("submit", function(e) {
+  $(".login-form").on("submit", function (e) {
     e.preventDefault();
     $(".username-valid").empty();
     $(".password-valid").empty();
@@ -11,22 +11,28 @@ $(function() {
       $(".password-valid").append("You must insert a password");
     } else {
       const user = {
-        username: $(".username-login")
-          .val()
-          .trim(),
-        password: $(".password-login")
-          .val()
-          .trim()
+        username: $(".username-login").val().trim(),
+        password: $(".password-login").val().trim(),
       };
       $.ajax("/api/auth", {
         type: "POST",
-        data: user
-      }).then(function(boolean) {
+        data: user,
+      }).then(function (boolean) {
         console.log(boolean);
         if (boolean === true) {
           window.location.href = "/profile-signup";
-        } else {
-          window.location.href = "/login-wrong";
+        } else if (boolean === false) {
+          $.ajax("/api/user/profile", {
+            type: "POST",
+            data: user,
+          }).then(function (boolean) {
+            if (boolean === true) {
+              window.location.href = "/homepage";
+            } else if (boolean === false) {
+              console.log("Wrong username or password");
+              window.location.href = "/login-wrong";
+            }
+          });
         }
       });
     }
--- routes/users-api-routes.js
+++ routes/users-api-routes.js
@@ -1,22 +1,22 @@
 const db = require("../models");
 const passport = require("../config/passport");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API routes for accessing users
 
   // Route for creating new users into the database
-  app.post("/api/users", function(req, res) {
+  app.post("/api/users", function (req, res) {
     const username = req.body.username;
     db.User.findAll({
       where: {
-        username: username
-      }
-    }).then(function(dbAuth) {
+        username: username,
+      },
+    }).then(function (dbAuth) {
       console.log(dbAuth);
       if (dbAuth.length) {
         return res.send(false);
       } else {
-        db.User.create(req.body).then(function(dbUser) {
+        db.User.create(req.body).then(function (dbUser) {
           return res.send(true);
         });
       }
@@ -24,74 +24,77 @@ module.exports = function(app) {
   });
 
   // Route for deleting users
-  app.delete("/api/users/:id", function(req, res) {
+  app.delete("/api/users/:id", function (req, res) {
     db.User.destroy({
       where: {
-        id: req.params.id
-      }
-    }).then(function(dbUser) {
+        id: req.params.id,
+      },
+    }).then(function (dbUser) {
       res.json(dbUser);
     });
   });
 
   // Route for authenticating login
-  app.post("/api/auth", passport.authenticate("local"), function(req, res) {
+  app.post("/api/auth", passport.authenticate("local"), function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
     console.log(username, password);
     db.User.findOne({
       where: {
         username: username,
-        password: password
-      }
-    }).then(function(dbUser) {
+        password: password,
+        has_profile: false,
+      },
+    }).then(function (dbUser) {
       if (dbUser === null) {
         res.send(false);
       } else {
         res.send(true);
       }
+    });
+  });
 
-      // else if (dbUser[0].has_profile === false) {
-      //   // If has_profile variable is false user is
-      //   // taken to the profile signup page
-      //   res.render("profile");
-      // } else {
-      //   // Else user's profile information is searched
-      //   // within the profile database
-      //   const username = dbUser[0].username;
-      //   db.Profile.findOne({
-      //     where: {
-      //       email: username
-      //     }
-      //   }).then(function(dbProfile) {
-      //     // Profile page is rendered through the
-      //     // database information in Profile
-      //     console.log(dbProfile);
-      //     res.render("user-profile", dbProfile[0]);
-      //   });
-      // }
-    });
-  });
   // Route for creating a user's profile
-  app.post("/api/profiles", function(req, res) {
-    db.Profile.create(req.body).then(function(dbProfile) {
-      console.log(dbProfile);
-      res.render("profile", dbProfile[0]);
+  app.post("/api/profiles", function (req, res) {
+    db.Profile.create(req.body).then(function (dbProfile) {
+      res.json(dbProfile);
     });
   });
+
+  // Route for getting user profile information
+  app.post("/api/user/profile", passport.authenticate("local"), function (
+    req,
+    res
+  ) {
+    const username = req.body.username;
+
+    db.Profile.findOne({
+      where: {
+        email: username,
+      },
+    }).then(function (dbProfile) {
+      console.log(dbProfile);
+      if (dbProfile === null) {
+        res.send(false);
+      } else {
+        res.send(true);
+      }
+    });
+  });
+
   // Route for updating user's has_profile value
-  app.put("/api/update", function(req, res) {
+  app.put("/api/update", function (req, res) {
     const username = req.body.email;
     const update = { has_profile: true };
     db.User.update(update, {
-      where: { username: username }
-    }).then(function(dbUser) {
+      where: { username: username },
+    }).then(function (dbUser) {
       res.json(dbUser);
     });
   });
 
   // Route for logging out
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
