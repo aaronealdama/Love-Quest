ca495f9e6d69d2729b1170c0943428746f8ec8e6
unknown <lovinmathew@hotmail.com>
2020-04-08 01:06:16 -0500

    Changed

--- models/user.js
+++ models/user.js
@@ -1,21 +1,21 @@
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    username: {
-      type: DataTypes.STRING,
-      validate: {
-        isEmail: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING
-    },
-    date_of_birth: {
-      type: DataTypes.STRING
-    },
-    has_profile: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    }
-  });
-  return User;
+    var User = sequelize.define("User", {
+        username: {
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING
+        },
+        date_of_birth: {
+            type: DataTypes.STRING
+        },
+        has_profile: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    }, { timestamp: false });
+    return User;
 };
