95478bdd28851623158ff6b6a11370be8c96b6e9
unknown <lovinmathew@hotmail.com>
2020-04-12 21:30:57 -0500

    Follow button

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,13 +1,13 @@
-$(function () {
-  // Getting profile information
-  $.ajax("/api/profile", {
-    type: "GET",
-  }).then(function (data) {
-    $(".welcome").text(`Welcome Back, ${data.first_name}!`);
-    $(".about-me").text(data.about_me);
-    $(".profile").attr("src", data.picture);
-    $(".para-name").text(`${data.first_name} ${data.last_name}`);
-    $(".para-email").text(data.email);
-    $(".para-marital").text(data.marital_status);
-  });
+$(function() {
+    // Getting profile information
+    $.ajax("/api/profile", {
+        type: "GET",
+    }).then(function(data) {
+        $(".welcome").text(`Welcome Back, ${data.first_name}!`);
+        $(".about-me").text(data.about_me);
+        $(".profile").attr("src", data.picture);
+        $(".para-name").text(`${data.first_name} ${data.last_name}`);
+        $(".para-email").text(data.email);
+        $(".para-marital").text(data.marital_status);
+    });
 });
---
+++ test-to test handlebars/followbutton.js
@@ -0,0 +1,11 @@
+//Follow Button Effect
+
+$(document).ready(
+
+    function iniciar() {
+        $('.follow').on("click", function() {
+            $('.follow').html('<div class="icon-heart"></div>  Following');
+        });
+    }
+
+);
